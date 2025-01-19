741d1214b2fbe97ecacb8ae25c130669024759ab
niodtn <71699228+niodtn@users.noreply.github.com>
2025-01-19 23:22:25 +0900

    Update main.js

--- toki/main.js
+++ toki/main.js
@@ -3,37 +3,49 @@
 // @namespace   Niodtn/Toki
 // @match       *://newtoki*.com/*
 // @grant       none
-// @version     1.0.6
+// @version     1.0.7
 // @author      Niodtn
 // @description Personal Tampermonkey script to filter content on newtoki.com
+// @run-at document-end
 // ==/UserScript==
 
 (function () {
   "use strict";
 
+  function getList(url) {
+    fetch(url)
+      .then((response) => response.text())
+      .then((data) => {
+        const remove = data
+          .split("\n")
+          .map((line) => line.trim())
+          .filter(Boolean);
+      })
+      .catch((error) => {
+        console.error("Niodtn/Toki: ", error);
+      });
+
+    return remove;
+  }
+
   const domain = window.location.hostname;
   if (/^newtoki\d+\.com$/.test(domain)) {
-    console.log("test");
-    document.addEventListener("DOMContentLoaded", () => {
-      const ulElement = document.querySelector("#webtoon-list-all ul");
-      if (ulElement) {
+    const ulElement = document.querySelector("#webtoon-list-all");
+    if (ulElement) {
+      console.log(ulElement);
+      let remove = getList(
+        "https://raw.githubusercontent.com/niodtn/scripts/refs/heads/dev/toki/newtoki/CN.txt"
+      );
+      remove.forEach((dt) => {
         const liElements = ulElement.querySelectorAll("li");
-        const url =
-          "https://raw.githubusercontent.com/niodtn/scripts/refs/heads/dev/toki/newtoki/CN.txt";
-        fetch(url)
-          .then((response) => response.text())
-          .then((data) => {
-            const remove = data.split("\n").map((line) => line.trim());
-            // data.split('\n').map(line => line.trim()).filter(Boolean);
 
-            remove.forEach((test) => {
-              console.log(test);
-            });
-          })
-          .catch((error) => {
-            console.error("Niodtn/Toki: ", error);
-          });
-      }
-    });
+        liElements.forEach((li) => {
+          const dateTitle = li.getAttribute("date-title");
+          if (dateTitle === dt) {
+            li.remove();
+          }
+        });
+      });
+    }
   }
 })();
