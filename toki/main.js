f0a0c14a8b9c41caa38a06a74eaf7c2a5fa2e7a5
niodtn <71699228+niodtn@users.noreply.github.com>
2025-01-19 21:58:32 +0900

    Update main.js

--- toki/main.js
+++ toki/main.js
@@ -7,3 +7,12 @@
 // @author      Niodtn
 // @description 1/19/2025, 9:41:47 PM
 // ==/UserScript==
+
+(function() {
+  'use strict';
+
+  const domain = window.location.hostname;
+  if (/^newtoki\d+\.com$/.test(domain)) {
+    console.log("test")
+  }
+})();
