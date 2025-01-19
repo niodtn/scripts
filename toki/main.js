04884fed81913b70c1c65aabace1dddd0ec85b99
niodtn <71699228+niodtn@users.noreply.github.com>
2025-01-19 22:48:42 +0900

    Update main.js

--- toki/main.js
+++ toki/main.js
@@ -3,35 +3,37 @@
 // @namespace   Niodtn/Toki
 // @match       *://newtoki*.com/*
 // @grant       none
-// @version     1.0.4
+// @version     1.0.5
 // @author      Niodtn
 // @description Personal Tampermonkey script to filter content on newtoki.com
 // ==/UserScript==
 
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
   const domain = window.location.hostname;
   if (/^newtoki\d+\.com$/.test(domain)) {
     console.log("test");
+    document.addEventListener("DOMContentLoaded", () => {
+      const ulElement = document.querySelector("#w-list ul");
+      if (ulElement) {
+        const liElements = ulElement.querySelectorAll("li");
+        const url =
+          "https://raw.githubusercontent.com/niodtn/scripts/refs/heads/dev/toki/newtoki/CN.txt";
+        fetch(url)
+          .then((response) => response.text())
+          .then((data) => {
+            const remove = data.split("\n").map((line) => line.trim());
+            // data.split('\n').map(line => line.trim()).filter(Boolean);
 
-    const ulElement = document.querySelector('#w-list ul');
-    if (ulElement) {
-      const liElements = ulElement.querySelectorAll('li');
-      const url = 'https://raw.githubusercontent.com/niodtn/scripts/refs/heads/dev/toki/newtoki/CN.txt';
-      fetch(url)
-        .then(response => response.text())
-        .then(data => {
-          const remove = data.split('\n').map(line => line.trim());
-          // data.split('\n').map(line => line.trim()).filter(Boolean);
-
-          remove.forEach(test => {
-            console.log(test);
+            remove.forEach((test) => {
+              console.log(test);
+            });
+          })
+          .catch((error) => {
+            console.error("Niodtn/Toki: ", error);
           });
-        })
-        .catch(error => {
-          console.error("Niodtn/Toki: ", error);
-        });
-    }
+      }
+    });
   }
 })();
