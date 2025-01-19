0d22ae572bd3f0cee895c063d1b262239cc760ef
niodtn <71699228+niodtn@users.noreply.github.com>
2025-01-19 22:21:42 +0900

    Update main.js

--- toki/main.js
+++ toki/main.js
@@ -3,7 +3,7 @@
 // @namespace   Niodtn/Toki
 // @match       *://newtoki*.com/*
 // @grant       none
-// @version     1.0.1
+// @version     1.0.2
 // @author      Niodtn
 // @description Personal Tampermonkey script to filter content on newtoki.com
 // ==/UserScript==
@@ -13,6 +13,20 @@
 
   const domain = window.location.hostname;
   if (/^newtoki\d+\.com$/.test(domain)) {
-    console.log("test")
-  }
+    console.log("test");
+
+    const url = 'https://raw.githubusercontent.com/niodtn/scripts/refs/heads/dev/toki/newtoki/CN.txt';
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        const remove = data.split('\n').map(line => line.trim()).filter(boolean);
+
+        remove.forEach(test => {
+          console.log(text);
+        });
+      })
+      .catch(error => {
+        console.error("Niodtn/Toki: ", error);
+      });
+  };
 })();
