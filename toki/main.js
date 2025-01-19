37d609b06a8744082e570b885652a7cc4c35934e
niodtn <71699228+niodtn@users.noreply.github.com>
2025-01-19 21:57:56 +0900

    Create main.js

---
+++ toki/main.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Toki autoHide
+// @namespace   Violentmonkey Scripts
+// @match       *://newtoki*.com/*
+// @grant       none
+// @version     1.0
+// @author      Niodtn
+// @description 1/19/2025, 9:41:47 PM
+// ==/UserScript==
