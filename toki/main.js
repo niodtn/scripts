986fdd675a32e298da6b30e7a6d855631f2421fc
niodtn <71699228+niodtn@users.noreply.github.com>
2025-01-20 10:23:51 +0900

    Update main.js

--- toki/main.js
+++ toki/main.js
@@ -5,7 +5,7 @@
 // @match       *://booktoki*.com/*
 // @include     /^https?:\/\/\w*toki\d*.com\/.*/
 // @grant       none
-// @version     1.2.0
+// @version     1.2.1
 // @author      Niodtn
 // @description Personal Tampermonkey script to filter content on newtoki.com
 // @run-at      document-end
