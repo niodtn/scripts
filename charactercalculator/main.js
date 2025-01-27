65b23a269195b9d3bd40d43275b7d092908ccd55
niodtn <71699228+niodtn@users.noreply.github.com>
2025-01-28 02:26:50 +0900

    Update main.js

--- charactercalculator/main.js
+++ charactercalculator/main.js
@@ -6,6 +6,9 @@
 // @version     1.0.0
 // @author      Niodtn
 // @description 좀 더 편하게 해줌
+// @run-at      document-end
+// @downloadURL https://raw.githubusercontent.com/niodtn/scripts/refs/heads/main/charactercalculator/main.js
+// @updateURL   https://raw.githubusercontent.com/niodtn/scripts/refs/heads/main/charactercalculator/main.js
 // ==/UserScript==
 
 (function() {
