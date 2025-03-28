49eebca24530362bda8ec7ccb8ab41ed1471c074
niodtn <71699228+niodtn@users.noreply.github.com>
2025-03-29 04:16:41 +0900

    Update main.js

--- chzzk/main.js
+++ chzzk/main.js
@@ -1,18 +1,34 @@
 // ==UserScript==
 // @name        Chzzk Helpful Script
 // @namespace   github:niodtn/scripts/chzzk
-// @match       *://newtoki*.com/*
-// @match       *://booktoki*.com/*
-// @include     /^https?:\/\/\w*toki\d*.com\/.*/
+// @match       *://chzzk.naver.com/live/*
 // @grant       none
 // @version     0.0.1
 // @author      Niodtn
-// @description Personal Tampermonkey script to filter content on newtoki.com
 // @run-at      document-end
+// @description Personal Tampermonkey script to filter content on newtoki.com
 // @downloadURL https://raw.githubusercontent.com/niodtn/scripts/refs/heads/chzzk/chzzk/main.js
 // @updateURL   https://raw.githubusercontent.com/niodtn/scripts/refs/heads/chzzk/chzzk/main.js
 // ==/UserScript==
 
+function removeChattingInput(target) {
+  console.log("test!");
+  console.log(target);
+}
+
 (function () {
   "use strict";
+
+  const observer = new MutationObserver((mutations, obs) => {
+    const target = document.querySelector("#aside-chatting");
+    if (target) {
+      removeChattingInput(target);
+      obs.disconnect();
+    }
+  });
+
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  });
 })();
