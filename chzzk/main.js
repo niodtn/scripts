dfe92e8ee1cc78d9763970bdd598615cb45fbed1
niodtn <71699228+niodtn@users.noreply.github.com>
2025-03-29 03:53:52 +0900

    Create main.js

---
+++ chzzk/main.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Toki autoHide
+// @namespace   github:niodtn/scripts/toki
+// @match       *://newtoki*.com/*
+// @match       *://booktoki*.com/*
+// @include     /^https?:\/\/\w*toki\d*.com\/.*/
+// @grant       none
+// @version     1.0.0
+// @author      Niodtn
+// @description Personal Tampermonkey script to filter content on newtoki.com
+// @run-at      document-end
+// @downloadURL https://raw.githubusercontent.com/niodtn/scripts/refs/heads/main/toki/main.js
+// @updateURL   https://raw.githubusercontent.com/niodtn/scripts/refs/heads/main/toki/main.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+})();
