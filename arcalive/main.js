f9bd3d18d137d966f9ce6e7ba7c07bc6a03dfd1a
niodtn <ipegte93@gmail.com>
2025-04-13 15:46:35 +0900

    Arcalive (#50)

--- arcalive/main.js
+++ arcalive/main.js
@@ -3,16 +3,13 @@
 // @namespace   github:niodtn/scripts/arcalive
 // @match       *://arca.live/b/*
 // @grant       none
-// @version     1.0.0
+// @version     1.0.1
 // @author      Niodtn
 // @description 아카라이브 Base64로 인코딩된 링크를 자동으로 디코딩
 // @run-at      document-end
 // @downloadURL https://raw.githubusercontent.com/niodtn/scripts/refs/heads/main/arcalive/main.js
 // @updateURL   https://raw.githubusercontent.com/niodtn/scripts/refs/heads/main/arcalive/main.js
 // ==/UserScript==
-
-const urlPattern = /https?/g;
-const base64Pattern = /(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}[A-Za-z0-9+/=]{0,2})?/g;
 
 const urlPattern = /https?:\/\//g;
 const base64Pattern =
