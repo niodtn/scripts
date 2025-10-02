e76be0595b4b0c9a16e928663d9b66996f2fcc34
niodtn <71699228+niodtn@users.noreply.github.com>
2025-10-03 03:08:01 +0900

    Chzzk (#60)

--- chzzk/main.user.js
+++ chzzk/main.user.js
@@ -1,67 +1,82 @@
 // ==UserScript==
-// @name        Chzzk Helpful Script
-// @namespace   github:niodtn/scripts/chzzk
-// @version     2025-10-02
-// @description Chzzk Custom
-// @author      Niodtn
-// @match       *://chzzk.naver.com/live/*
-// @grant       none
-// @run-at      document-end
-// @downloadURL https://niodtn.github.io/scripts/chzzk/main.user.js
-// @updateURL   https://niodtn.github.io/scripts/chzzk/main.user.js
+// @name         Chzzk Helpful Script
+// @namespace    github:niodtn/scripts/chzzk
+// @version      2025-10-03
+// @description  Chzzk Custom
+// @author       Niodtn
+// @match        https://chzzk.naver.com/*
+// @icon         https://ssl.pstatic.net/static/nng/glive/icon/favicon.png
+// @grant        none
+// @run-at       document-end
+// @downloadURL  https://niodtn.github.io/scripts/chzzk/main.user.js
+// @updateURL    https://niodtn.github.io/scripts/chzzk/main.user.js
 // ==/UserScript==
 
-function findAndRun(func, target) {
-  const runFunc = () => {
-    const _target = document.querySelector(target);
-    if (_target) func(_target);
-  };
+function addcss() {
+  const imgs = document.querySelectorAll('img[alt="채팅 운영자"]');
+  imgs.forEach((img) => {
+    const parent = img.closest(
+      'div[class^="live_chatting_message_chatting_message"]'
+    );
 
-  runFunc();
+    if (!parent) return;
+    if (parent.dataset.cssApplied) return;
 
-  const observer = new MutationObserver(runFunc);
-  observer.observe(document, { childList: true, subtree: true });
+    // background
+    parent.style.backgroundColor = "#749ffe";
+    parent.style.borderRadius = "7px";
+    parent.style.backgroundColor;
 
-  const originalPushState = history.pushState;
-  const originalReplaceState = history.replaceState;
+    // nickname
+    const nickname = parent.querySelector(
+      'span[class^="live_chatting_username_nickname"]'
+    );
+    nickname.style.color = "#fff";
 
-  const onUrlChange = () => {
-    runFunc();
-  };
+    // message
+    const messageText = parent.querySelector(
+      'span[class^="live_chatting_message_text"]'
+    );
+    messageText.style.color = "#fff";
 
-  history.pushState = function (...args) {
-    originalPushState.apply(this, args);
-    onUrlChange();
-  };
-
-  history.replaceState = function (...args) {
-    originalReplaceState.apply(this, args);
-    onUrlChange();
-  };
-
-  window.addEventListener("popstate", onUrlChange);
+    parent.dataset.cssApplied = "true";
+  });
 }
 
-function removeElement(el) {
-  // el.remove();
-  el.style.display = "none";
+function hideElement(target) {
+  const _target = document.querySelector(target);
+  if (_target) _target.style.display = "none";
 }
 
-function alignToCenter(el) {
-  el.style.display = "flex";
-  el.style.justifyContent = "center";
-  el.style.alignItems = "center";
-  el.style.minHeight = "auto";
+function chatting() {
+  const inputContainer = document.querySelector(
+    'div[class^="live_chatting_input_container"]'
+  );
+  if (inputContainer) inputContainer.style.display = "none";
+
+  const button = document.querySelector("#send_chat_or_donate");
+  if (button) button.style.display = "none";
+
+  const area = document.querySelector("[class^='live_chatting_area'");
+  if (area) {
+    area.style.display = "flex";
+    area.style.justifyContent = "center";
+    area.style.alignItems = "center";
+    area.style.minHeight = "auto";
+  }
 }
 
 (function () {
   "use strict";
 
-  // Remove Popup
-  findAndRun(removeElement, "[class^='popup_dimmed']");
-
-  // Remove Chatting Input
-  findAndRun(removeElement, "[class^='live_chatting_input_container']");
-  findAndRun(removeElement, "#send_chat_or_donate");
-  findAndRun(alignToCenter, "[class^='live_chatting_area'");
+  const observer = new MutationObserver(() => {
+    if (
+      location.pathname.startsWith("/live") ||
+      location.pathname.startsWith("/video")
+    ) {
+      addcss();
+      chatting();
+    }
+  });
+  observer.observe(document.body, { childList: true, subtree: true });
 })();
