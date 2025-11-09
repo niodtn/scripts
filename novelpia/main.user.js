2be7d643aa939095fc9defb2c361ffd885572ae8
niodtn <71699228+niodtn@users.noreply.github.com>
2025-11-10 01:05:39 +0900

    Update main.user.js

--- novelpia/main.user.js
+++ novelpia/main.user.js
@@ -1,28 +1,34 @@
 // ==UserScript==
 // @name         Novelpia Script
 // @namespace    github:niodtn/scripts/novelpia
-// @version      2025-11-09
+// @version      2025-11-10
 // @description  Novelpia
 // @author       Niodtn
 // @match        *://novelpia.com/*
 // @match        *://www.novelpia.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=novelpia.com
-// @grant        none
+// @grant        GM.xmlHttpRequest
 // @run-at       document-end
 // @downloadURL  https://niodtn.github.io/scripts/novelpia/main.user.js
 // @updateURL    https://niodtn.github.io/scripts/novelpia/main.user.js
 
+// @connect      script.google.com
+// @connect      script.googleusercontent.com
+
 // @require      https://niodtn.github.io/scripts/lib/router.js
 // @require      https://niodtn.github.io/scripts/novelpia/mybook.js
+// @require      https://niodtn.github.io/scripts/novelpia/novel.js
 // ==/UserScript==
 
-/* global PageDispatcher, mybook */
+/* global PageDispatcher, mybook, novel */
 
 (function () {
   "use strict";
+
   const dispatcher = new PageDispatcher();
 
   dispatcher.register("/mybook", mybook);
+  dispatcher.register("/novel", novel);
 
   dispatcher.start();
 })();
