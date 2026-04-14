ecd255a13177b3bd351174ff73522102b5191248
niodtn <ipegte93@gmail.com>
2026-04-15 01:45:55 +0900

    update

--- dailykillersudoku/main.user.js
+++ dailykillersudoku/main.user.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         My Sudoku Script
 // @namespace    github:niotn/scripts/dailykillersudoku
-// @version      2026-04-15c
+// @version      2026-04-15d
 // @description  Grid Layout for Pencil Marks
 // @author       Niodtn
 // @match        https://www.dailykillersudoku.com/puzzle/*
 // @match        https://www.dailykillersudoku.com/m/puzzle/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_addStyle
-
+//
+// @downloadURL  https://niodtn.github.io/scripts/dailykillersudoku/main.user.js
+// @updateURL    https://niodtn.github.io/scripts/dailykillersudoku/main.user.js
+//
 // @require      https://niodtn.github.io/scripts/lib/router.js
 // ==/UserScript==
 
@@ -48,7 +51,9 @@ function applyGrid(cell) {
 
       /* grid layout */
       grid-template-columns: repeat(3, 1fr);
-      grid-template-rows: repeat(3, 1fr);
+      grid-template-rows: repeat(3, 1fr) !important;
+      align-content: stretch !important;
+
       width: 100% !important;
       height: 100% !important;
 
@@ -69,6 +74,18 @@ function applyGrid(cell) {
 
     :root.dark-mode .grid-num {
       color: #eef !important;
+    }
+
+    @media screen and (max-width: 768px) {
+      .pencil-marks {
+        padding: 12px 4px 4px 4px !important;
+      }
+
+      .grid-num {
+        font-size: 80% !important;
+        max-height: 100%;
+        overflow: hidden;
+      }
     }
   `);
 
